f269bd1e39c64f95feba335ff265b064703d06b5
Vinicius Vicente <vinicius.vicente@evino.com.br>
2015-06-21 23:09:10 -0300

    partial upload

--- app/controllers/import_controller.rb
+++ app/controllers/import_controller.rb
@@ -1,6 +1,11 @@
+require 'json'
 
 class ImportController < ApplicationController
-  get '/' do
+  get '/upload' do
     haml :upload
+  end
+  post "/upload" do
+    content_type :json
+    return JSON.pretty_generate(params)
   end
 end
